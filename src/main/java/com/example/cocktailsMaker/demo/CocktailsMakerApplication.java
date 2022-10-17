239120e056a7c06e6d9112ad87e3fdad7455e5f5
apolon15 <ya.apolon15@yandex.ru>
2022-10-17 21:38:23 +0300

    Initial commit

---
+++ src/main/java/com/example/cocktailsMaker/demo/CocktailsMakerApplication.java
@@ -0,0 +1,13 @@
+package com.example.cocktailsMaker.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CocktailsMakerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CocktailsMakerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cocktailsMaker/demo/CocktailsMakerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cocktailsMaker.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CocktailsMakerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
