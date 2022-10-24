5fff31cc1dc81bffd458b6ef7d3cdc5d389704c3
apolon15 <ya.apolon15@yandex.ru>
2022-10-24 21:48:47 +0300

    Работает форма

--- src/main/java/com/example/cocktailsMaker/demo/controler/UserController.java
+++ src/main/java/com/example/cocktailsMaker/demo/controler/UserController.java
@@ -1,12 +1,20 @@
 package com.example.cocktailsMaker.demo.controler;
 
+import com.example.cocktailsMaker.demo.models.Alcohole;
+import com.example.cocktailsMaker.demo.models.Syrups;
 import com.example.cocktailsMaker.demo.models.User;
+import com.example.cocktailsMaker.demo.models.Zapivon;
+import com.example.cocktailsMaker.demo.repository.AlcoholeRepository;
+import com.example.cocktailsMaker.demo.repository.SyrupsRepository;
+import com.example.cocktailsMaker.demo.repository.ZapivonRepository;
 import com.example.cocktailsMaker.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 
