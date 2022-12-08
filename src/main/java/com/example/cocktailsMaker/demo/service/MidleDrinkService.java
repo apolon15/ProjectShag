59b5c00471857ae509df11bd50c0714700ded4f9
apolon15 <ya.apolon15@yandex.ru>
2022-12-08 22:18:49 +0300

    Интегрирован Youtube

--- src/main/java/com/example/cocktailsMaker/demo/service/MidleDrinkService.java
+++ src/main/java/com/example/cocktailsMaker/demo/service/MidleDrinkService.java
@@ -1,8 +1,7 @@
 package com.example.cocktailsMaker.demo.service;
 
 import com.example.cocktailsMaker.demo.models.MidleDrink;
-import com.example.cocktailsMaker.demo.repository.MidleDrinkRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+
 
 import java.util.List;
 
